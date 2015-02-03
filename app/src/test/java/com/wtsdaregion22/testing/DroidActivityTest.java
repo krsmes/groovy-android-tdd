576d6848786947fae36aa0ae0b5c0e749d786c44
Mark Holtman <mholtman@gmail.com>
2015-02-03 13:40:50 -0500

    Two failing tests,but robolectric is doing its job.

--- app/src/test/java/com/wtsdaregion22/testing/DroidActivityTest.java
+++ app/src/test/java/com/wtsdaregion22/testing/DroidActivityTest.java
@@ -3,17 +3,32 @@ package com.wtsdaregion22.testing;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import android.widget.TextView;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(emulateSdk = 21)
 public class DroidActivityTest {
 
     @Test
     public void shouldHaveAHelloGroovyString() throws Exception {
         String hello = new DroidActivity().getResources().getString(R.string.hello_world);
         assertThat(hello, equalTo("Hello world!"));
+    }
+
+    @Test
+    public void shouldCheckTextViewHasCorrectText() throws Exception {
+        String expected = "Hello world.. now with Groovy!";
+        DroidActivity myActivity = new DroidActivity();
+
+        TextView actualView = (TextView) myActivity.findViewById(R.id.message);
+
+        String actual = actualView.getText().toString();
+        assertThat(expected, equalTo(actual));
     }
 }
